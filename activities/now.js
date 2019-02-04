7be5c280f9d49d817110d638ea76d8685ea58a28
olliejm <olliejm@protonmail.com>
2019-02-04 16:45:09 +0000

    update for new logger and provider, more sensible utils example

--- activities/now.js
+++ activities/now.js
@@ -4,18 +4,8 @@ const utils = require('./common/utils');
 
 module.exports = async (activity) => {
     try {
-        activity.Response.Data = utils.now();
+        activity.Response.Data = new Date().toISOString();
     } catch (error) {
-        let m = error.message;
-
-        if (error.stack) {
-            m = m + ': ' + error.stack;
-        }
-
-        activity.Response.ErrorCode = (error.response && error.response.statusCode) || 500;
-
-        activity.Response.Data = {
-            ErrorText: m
-        };
+        utils.handleError(error, activity);
     }
 };
